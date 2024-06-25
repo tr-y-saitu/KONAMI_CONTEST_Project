6175084ce859801da48e580027c3039e90e7b4a0
izabera777 <150010641+izabera777@users.noreply.github.com>
2024-06-25 10:03:01 +0900

    島クラス作成

---
+++ Island.h
@@ -0,0 +1,35 @@
+﻿#pragma once
+#include "Common.h"
+
+/// <summary>
+/// 島
+/// </summary>
+class Island
+{
+public:
+    /// <summary>
+    /// コンストラクタ
+    /// </summary>
+    Island();
+
+    /// <summary>
+    /// デストラクタ
+    /// </summary>
+    ~Island();
+
+    /// <summary>
+    /// 更新
+    /// </summary>
+    void Update();
+
+    /// <summary>
+    /// 描画
+    /// </summary>
+    void Draw();
+
+private:
+    int     modelHandle;        // モデルハンドル
+    VECTOR  position;           // 座標
+    VECTOR  scale;              // スケール
+};
+
