fc487e3f63f00c22d994afc468e93a5f5e4c7425
izabera777 <150010641+izabera777@users.noreply.github.com>
2024-06-25 15:12:08 +0900

    エフェクトデータ名が日本語だったため修正

--- EffectManager.cpp
+++ EffectManager.cpp
@@ -58,7 +58,7 @@ EffectManager::~EffectManager()
 void EffectManager::LoadEffect()
 {
     gemGetEffect = LoadEffekseerEffect("data/effect/getEffectLight.efk", 0.3f);
-    playerHitEffect = LoadEffekseerEffect("data/effect/playerHitEffect波紋.efk", 2.0f);
+    playerHitEffect = LoadEffekseerEffect("data/effect/playerHitWaveEffect.efk", 2.0f);
     gemFallEffect = LoadEffekseerEffect("data/effect/gemFallEffect.efk", 2.0f);
 }
 
