699546fd6c05198ad9977c5625c3877d028f9528
izabera777 <150010641+izabera777@users.noreply.github.com>
2024-07-03 09:51:47 +0900

    使用する音を書き出し

--- SoundManager.h
+++ SoundManager.h
@@ -51,7 +51,27 @@ private:
     vector<int> playingList;                // 現在再生中のサウンドリスト
 
     //                  サウンドデータ                  //
+    // 共通
+    int     pushuSE;            // ボタンを押したときの音
+
+    // タイトルシーン
     int     titleSceneBGM;      // タイトルシーンのBGM
+
+    // ゲームシーン
+    int     gameSceneBGM;       // ゲームシーンのBGM
+    int     gemHitSE;           // 宝石とプレイヤーが当たった時の音
+    int     gemGetSE;           // 宝石を獲得した時の音
+    int     rainSE;             // 雨の音
+    int     wavesSE;            // 波の音
+    int     burningSE;          // 燃える音
+
+    // クリアシーン
+    int     clearSceneBGM;      // クリアシーンのBGM
+    int     fireworksSE;        // 花火の音
+
+
+
+
 };
 
 
